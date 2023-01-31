82578457bfee1555a50c7b60c1fc0a4769da42e7
Helmut Drewes <helmut@contagra.com>
2023-01-31 09:55:44 +0200

    [IMP] base_geoengine: Add POINT import converter

--- base_geoengine/models/ir_fields_converter.py
+++ base_geoengine/models/ir_fields_converter.py
@@ -24,3 +24,14 @@ class IrFieldsConverter(models.AbstractModel):
                 _(u"'%s' does not seem to be an geometry for field '%%(field)s'"),
                 value
             )
+
+    @api.model
+    def _str_to_geo_point(self, model, field, value):
+        try:
+            return value, []
+        except ValueError:
+            raise self._format_import_error(
+                ValueError,
+                _(u"'%s' does not seem to be an geometry for field '%%(field)s'"),
+                value
+            )
