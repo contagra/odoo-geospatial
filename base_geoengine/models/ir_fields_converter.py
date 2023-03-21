15f087430052913fad854ec0c7785ccb2dbb684b
Helmut Drewes <helmut@contagra.com>
2023-03-21 07:21:47 +0200

    [IMP] base_geoengine: Add polygon converter

--- base_geoengine/models/ir_fields_converter.py
+++ base_geoengine/models/ir_fields_converter.py
@@ -13,7 +13,18 @@ class IrFieldsConverter(models.AbstractModel):
         except ValueError:
             raise self._format_import_error(
                 ValueError,
-                _(u"'%s' does not seem to be an geometry for field '%%(field)s'"),
+                _(u"'%s' does not seem to be a geometry for field '%%(field)s'"),
+                value
+            )
+
+    @api.model
+    def _str_to_geo_polygon(self, model, field, value):
+        try:
+            return value, []
+        except ValueError:
+            raise self._format_import_error(
+                ValueError,
+                _(u"'%s' does not seem to be a geometry for field '%%(field)s'"),
                 value
             )
 
