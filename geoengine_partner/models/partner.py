73a4e423a0b392f17e9ceeb2352dc77332a93990
Helmut Drewes <helmut.drewes@agrista.com>
2023-11-29 09:23:00 +0200

    [IMP] geoengine_partner: rename to location

--- geoengine_partner/models/partner.py
+++ geoengine_partner/models/partner.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    """Add geo_point to partner using a function filed"""
+    """Add location to partner using a function filed"""
 
     _inherit = "res.partner"
 
-    geo_point = fields.GeoPoint("Address coordinates")
+    location = fields.GeoPoint("Location")
--- geoengine_partner/models/res_partner.py
+++ geoengine_partner/models/res_partner.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    """Add geo_point to partner using a function filed"""
+    """Add location to partner using a function filed"""
 
     _inherit = "res.partner"
 
